079d05bc3877825299a74ac515b95475ece80704
ly49nkallo <tyabrennan@gmail.com>
2026-06-21 00:21:59 -0700

    r

---
+++ serve.py
@@ -0,0 +1,16 @@
+
+"""Start a local HTTP server for previewing the site at http://localhost:8000"""
+
+import http.server
+import socketserver
+import webbrowser
+
+PORT = 8000
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    print("Press Ctrl+C to stop.")
+    webbrowser.open(f"http://localhost:{PORT}")
+    httpd.serve_forever()
